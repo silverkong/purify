1a488b8e354ee98129493e80f7959591335c77d2
drimh <107299318+drimh@users.noreply.github.com>
2023-12-09 02:50:04 +0900

    feat: first setting

---
+++ frontend/pages/Login.tsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import React from 'react';
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+        fd
+    </div>
+  );
+};
+
+export default Login;
