8d5f565f80689ec4a747aa68bd8f67bada215f1a
drimh <107299318+drimh@users.noreply.github.com>
2023-12-11 01:11:02 +0900

    feat: profile thumb 추가

--- frontend/components/ProfileTop.tsx
+++ frontend/components/ProfileTop.tsx
@@ -1,5 +1,18 @@
 import React from "react"
 import styles from "../styles/Profile.module.css"
+import like from "../image/like.png"
+import dislike from "../image/dislike.png"
+
+const thumbStyle = { 
+  color: "#444", 
+  fontFamily: "Poppins",
+  fontSize: "1.125rem",
+  fontStyle: "normal",
+  fontWeight: "500",
+  lineHeight: "0.9375rem", 
+  marginLeft:"0.94rem", 
+  marginRight:"1.88rem"
+}
 
 const ProfileTop = ({ name, pfp, principal }) => {
   return (
@@ -19,6 +32,10 @@ const ProfileTop = ({ name, pfp, principal }) => {
         <div className={styles.box_profile_info_bottom}>
           <h1>0</h1>
           <span>point</span>
+              <img src={like} style={{marginLeft:"1.88rem"}}/>
+            <span style={thumbStyle}>3</span>
+              <img src={dislike}/>
+            <span style={thumbStyle}>1</span>
         </div>
       </div>
     </section>
